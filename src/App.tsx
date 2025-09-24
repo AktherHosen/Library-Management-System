86ff4aab859f677ab7148a6b836f654bc0c040e5
aktherhosen <dev.aktherhosen@gmail.com>
2025-09-24 23:59:01 +0600

    added: navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { useState } from "react";
 import "./App.css";
-import { Button } from "./components/ui/button";
+import Home from "./pages/home/home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <Button>Click me</Button>
+      <Home />
     </>
   );
 }
---
+++ src/pages/home/navbar.tsx
@@ -0,0 +1,30 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+const Navbar = () => {
+  return (
+    <div className="flex justify-between items-center">
+      <h4>Bookify</h4>
+      <div>
+        {" "}
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <NavigationMenuLink>Link</NavigationMenuLink>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
