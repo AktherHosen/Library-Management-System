762cce00ddced82193ab38bae4ecb634a395d93b
aktherhosen <dev.aktherhosen@gmail.com>
2025-09-29 21:34:32 +0600

    update url

--- src/redux/api/lmsApi.ts
+++ src/redux/api/lmsApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const lmsApi = createApi({
   reducerPath: "lmsApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: `${import.meta.env.VITE_API_URL}/api`,
+    baseUrl: `${import.meta.env.VITE_API_URL}`,
   }),
   tagTypes: ["books", "borrow"],
   endpoints: (builder) => ({
